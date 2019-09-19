c4911347cb5b01ab3054ce8169c557ccbdb36b20
Tobias Brumhard <Tobias.Brumhard@mail.schwarz>
2019-09-19 08:11:33 +0200

    default current path in plugin_loader

--- src/plugins/plugin_loader.py
+++ src/plugins/plugin_loader.py
@@ -1,10 +1,14 @@
 import importlib
+from pathlib import Path
 
 
 class Plugin_Loader:
     @staticmethod
-    def load(path, class_name):
-        spec = importlib.util.spec_from_file_location("plugin", path)
+    def load(class_name, path = None):
+        if path == None:
+            current_path = Path(__file__).parent
+            abs_file_path = Path(current_path).joinpath(f"{class_name}.py")
+        spec = importlib.util.spec_from_file_location("plugin", str(abs_file_path))
         module = importlib.util.module_from_spec(spec)
         spec.loader.exec_module(module)
         class_ = getattr(module, class_name)
